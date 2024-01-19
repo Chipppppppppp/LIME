5e666349cd236eed45564d9c1841c8cc216e17f6
Chipppppp <Chippppppppppppppp@gmail.com>
2024-01-19 13:42:24 +0900

    Fix

---
+++ app/src/main/java/com/example/lime/Main.java
@@ -0,0 +1,81 @@
+package com.example.lime;
+
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.view.View;
+import android.view.ViewGroup;
+import android.net.Uri;
+import android.webkit.WebView;
+import androidx.browser.customtabs.CustomTabsIntent;
+import android.content.Intent;
+
+public class Main implements IXposedHookLoadPackage {
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lparam) throws Throwable {
+        XSharedPreferences prefs = new XSharedPreferences("com.example.lime", "settings");
+        prefs.reload();
+        boolean deleteVoom = prefs.getBoolean("delete_voom", true);
+        boolean deleteAds = prefs.getBoolean("delete_ads", true);
+        boolean redirectWebView = prefs.getBoolean("redirect_web_view", true);
+        boolean openInBrowser = prefs.getBoolean("open_in_browser", false);
+
+        Class hookTarget;
+
+        if (deleteVoom) {
+            hookTarget = lparam.classLoader.loadClass("jp.naver.line.android.activity.main.MainActivity");
+            XposedHelpers.findAndHookMethod(hookTarget, "onResume", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    ((ViewGroup) ((Activity) param.thisObject).findViewById(2131433343)).getChildAt(6).setVisibility(View.GONE);
+                }
+            });
+        }
+
+        if (deleteAds) {
+            hookTarget = lparam.classLoader.loadClass("com.linecorp.line.ladsdk.ui.common.view.lifecycle.LadAdView");
+            XposedHelpers.findAndHookMethod(hookTarget, "onAttachedToWindow", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    View view = ((View) ((View) param.thisObject).getParent().getParent().getParent());
+                    ((ViewGroup) view.getParent()).removeView(view);
+                }
+            });
+            hookTarget = lparam.classLoader.loadClass("com.linecorp.line.admolin.smartch.v2.view.SmartChannelViewLayout");
+            XposedHelpers.findAndHookMethod(hookTarget, "dispatchDraw", Canvas.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    ((View) ((View) param.thisObject).getParent()).setVisibility(View.GONE);
+                }
+            });
+        }
+
+        if (redirectWebView) {
+            hookTarget = lparam.classLoader.loadClass("android.webkit.WebView");
+            XposedBridge.hookAllMethods(hookTarget, "loadUrl", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    WebView webView = (WebView) param.thisObject;
+                    Activity activity = (Activity) webView.getContext();
+                    webView.setVisibility(View.GONE);
+                    webView.stopLoading();
+                    Uri uri = Uri.parse((String) param.args[0]);
+                    if (openInBrowser) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(uri);
+                        activity.startActivity(intent);
+                    } else {
+                        CustomTabsIntent tabsIntent = new CustomTabsIntent.Builder().setShowTitle(true).build();
+                        tabsIntent.launchUrl(activity, uri);
+                    }
+                    activity.finish();
+                }
+            });
+        }
+    }
+}
