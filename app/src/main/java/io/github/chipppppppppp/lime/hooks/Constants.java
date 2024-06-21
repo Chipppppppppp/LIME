9e60f45cbd05215bc4f5a2b3c5bd692987d55362
Chippppp <78024852+Chipppppppppp@users.noreply.github.com>
2024-06-21 13:10:20 +0900

    Support LINE 14.9.1

--- app/src/main/java/io/github/chipppppppppp/lime/hooks/Constants.java
+++ app/src/main/java/io/github/chipppppppppp/lime/hooks/Constants.java
@@ -14,12 +14,12 @@ public class Constants {
         }
     }
 
-    static final HookTarget USER_AGENT_HOOK = new HookTarget("bZ0.c", "h");
-    static final HookTarget WEBVIEW_CLIENT_HOOK = new HookTarget("hz0.l", "onPageFinished");
-    static final HookTarget COMMUNICATION_ENUM_HOOK = new HookTarget("n01.Z5", "b");
-    static final HookTarget UNSENT_HOOK = new HookTarget("IZ0.b", "p");
-    static final HookTarget MUTE_MESSAGE_HOOK = new HookTarget("YX0.b", "G");
-    static final HookTarget MARK_AS_READ_HOOK = new HookTarget("mJ.d$d", "run");
+    static final HookTarget USER_AGENT_HOOK = new HookTarget("q01.c", "h");
+    static final HookTarget WEBVIEW_CLIENT_HOOK = new HookTarget("xA0.k", "onPageFinished");
+    static final HookTarget COMMUNICATION_ENUM_HOOK = new HookTarget("D11.Z5", "b");
+    static final HookTarget UNSENT_HOOK = new HookTarget("h41.a", "p");
+    static final HookTarget MUTE_MESSAGE_HOOK = new HookTarget("nZ0.b", "G");
+    static final HookTarget MARK_AS_READ_HOOK = new HookTarget("BJ.d$d", "run");
     static final HookTarget REQUEST_HOOK = new HookTarget("org.apache.thrift.l", "b");
     static final HookTarget RESPONSE_HOOK = new HookTarget("org.apache.thrift.l", "a");
 }
