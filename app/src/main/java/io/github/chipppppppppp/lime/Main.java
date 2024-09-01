74bc2bb03374e6858b92cddfe39cc082ee6ce656
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2024-09-01 19:45:06 +0900

    Update Main.java

--- app/src/main/java/io/github/chipppppppppp/lime/Main.java
+++ app/src/main/java/io/github/chipppppppppp/lime/Main.java
@@ -32,6 +32,7 @@ import io.github.chipppppppppp.lime.hooks.RemoveReplyMute;
 import io.github.chipppppppppp.lime.hooks.SendMuteMessage;
 import io.github.chipppppppppp.lime.hooks.SpoofAndroidId;
 import io.github.chipppppppppp.lime.hooks.SpoofUserAgent;
+import io.github.chipppppppppp.lime.hooks.Unsentrec;
 
 public class Main implements IXposedHookLoadPackage, IXposedHookInitPackageResources, IXposedHookZygoteInit {
     public static String modulePath;
@@ -61,7 +62,8 @@ public class Main implements IXposedHookLoadPackage, IXposedHookInitPackageResou
             new KeepUnread(),
             new BlockTracking(),
             new ModifyResponse(),
-            new OutputRequest()
+            new OutputRequest(),
+            new Unsentrec()
     };
 
     public void handleLoadPackage(@NonNull XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
