e1b05b7979909d631816bf52c1f93d5e251a8559
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2024-10-04 19:49:05 +0900

    Update Constants.java
    ミュートメッセージのメゾットの変更ミスを訂正
    動作確認済み

--- app/src/main/java/io/github/chipppppppppp/lime/hooks/Constants.java
+++ app/src/main/java/io/github/chipppppppppp/lime/hooks/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
 
     static final HookTarget USER_AGENT_HOOK = new HookTarget("C81.c", "h");
     static final HookTarget WEBVIEW_CLIENT_HOOK = new HookTarget("qH0.m", "onPageFinished");
-    static final HookTarget MUTE_MESSAGE_HOOK = new HookTarget("u71.b", "G");
+    static final HookTarget MUTE_MESSAGE_HOOK = new HookTarget("u71.b", "H");
     static final HookTarget MARK_AS_READ_HOOK = new HookTarget("JL.e$d", "run");
     static final HookTarget REQUEST_HOOK = new HookTarget("org.apache.thrift.k", "b");
     static final HookTarget RESPONSE_HOOK = new HookTarget("org.apache.thrift.k", "a");
