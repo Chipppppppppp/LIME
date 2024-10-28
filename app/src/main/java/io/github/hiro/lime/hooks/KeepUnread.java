c9abca032dc79bda71718af52ad03d81dd105421
areteruhiro <narumihiroki1114@gmail.com>
2024-10-29 00:15:49 +0900

    保存方法の変更　Ver2

--- app/src/main/java/io/github/hiro/lime/hooks/KeepUnread.java
+++ app/src/main/java/io/github/hiro/lime/hooks/KeepUnread.java
@@ -1,32 +1,23 @@
 package io.github.hiro.lime.hooks;
-import android.app.AndroidAppHelper;
 import android.content.Context;
 
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
-import android.widget.TextView;
-
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import io.github.hiro.lime.LimeOptions;
-import io.github.hiro.lime.R;
 
 public class KeepUnread implements IHook {
 
     @Override
     public void hook(LimeOptions limeOptions, XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         if (limeOptions.removeKeepUnread.checked) return;
-
-
 
         XposedHelpers.findAndHookMethod(
                 "com.linecorp.line.chatlist.view.fragment.ChatListFragment",
@@ -35,19 +26,15 @@ public class KeepUnread implements IHook {
                 LayoutInflater.class, ViewGroup.class, android.os.Bundle.class,
                 new XC_MethodHook() {
                     @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    protected void afterHookedMethod(MethodHookParam param) {
                         View rootView = (View) param.getResult();
                         Context context = rootView.getContext();
-                        Context moduleContext = AndroidAppHelper.currentApplication().createPackageContext(
-                                "io.github.hiro.lime", Context.CONTEXT_IGNORE_SECURITY);
 
-                        // レイアウトを作成
                         RelativeLayout layout = new RelativeLayout(context);
                         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
                                 RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
                         layout.setLayoutParams(layoutParams);
 
-                        // スイッチを作成
                         Switch switchView = new Switch(context);
                         switchView.setText("");
                         switchView.setTextColor(Color.WHITE);
@@ -57,16 +44,10 @@ public class KeepUnread implements IHook {
                         switchParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
 
                         switchView.setChecked(false);
-
-
-                        switchView.setOnCheckedChangeListener((buttonView, isChecked1) -> {
-
+                        switchView.setOnCheckedChangeListener((buttonView, isChecked) -> {
                         });
-
-                        // レイアウトにスイッチを追加
                         layout.addView(switchView, switchParams);
 
-                        // 既存のビューにレイアウトを追加
                         if (rootView instanceof ViewGroup) {
                             ViewGroup rootViewGroup = (ViewGroup) rootView;
                             if (rootViewGroup.getChildCount() > 0 && rootViewGroup.getChildAt(0) instanceof ListView) {
@@ -80,14 +61,12 @@ public class KeepUnread implements IHook {
                 }
         );
 
-
-
         XposedHelpers.findAndHookMethod(
                 loadPackageParam.classLoader.loadClass(Constants.MARK_AS_READ_HOOK.className),
                 Constants.MARK_AS_READ_HOOK.methodName,
                 new XC_MethodHook() {
                     @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    protected void beforeHookedMethod(MethodHookParam param) {
                         param.setResult(null);
                     }
                 }
