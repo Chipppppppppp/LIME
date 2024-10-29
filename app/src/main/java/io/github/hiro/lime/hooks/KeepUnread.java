eb4dbce4ad7bde5e4faa42d461636889f4201bb1
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2024-10-29 20:49:38 +0900

    Update KeepUnread.java

--- app/src/main/java/io/github/hiro/lime/hooks/KeepUnread.java
+++ app/src/main/java/io/github/hiro/lime/hooks/KeepUnread.java
@@ -1,88 +1,87 @@
-package io.github.hiro.lime.hooks;
-import android.content.Context;
+@Override
+public void hook(LimeOptions limeOptions, XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+    if (limeOptions.removeKeepUnread.checked) return;
 
-import android.graphics.Color;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.Switch;
+    XposedHelpers.findAndHookMethod(
+            "com.linecorp.line.chatlist.view.fragment.ChatListFragment",
+            loadPackageParam.classLoader,
+            "onCreateView",
+            LayoutInflater.class, ViewGroup.class, android.os.Bundle.class,
+            new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    View rootView = (View) param.getResult();
+                    Context context = rootView.getContext();
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+                    keepUnread = readStateFromFile(context);
+                    
+                    RelativeLayout layout = new RelativeLayout(context);
+                    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                            RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                    layout.setLayoutParams(layoutParams);
 
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import io.github.hiro.lime.LimeOptions;
+                    Switch switchView = new Switch(context);
+                    switchView.setText(""); 
+                    switchView.setTextColor(Color.WHITE); 
+                    switchView.setChecked(keepUnread);
 
-public class KeepUnread implements IHook {
-    static boolean keepUnread = false;
+                    switchView.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                        keepUnread = isChecked; 
+                        saveStateToFile(context, isChecked); 
+                    });
 
-    @Override
-    public void hook(LimeOptions limeOptions, XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        if (limeOptions.removeKeepUnread.checked) return;
+                    RelativeLayout.LayoutParams switchParams = new RelativeLayout.LayoutParams(
+                            RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                    switchParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                    layout.addView(switchView, switchParams);
 
-
-        XposedHelpers.findAndHookMethod(
-                "com.linecorp.line.chatlist.view.fragment.ChatListFragment",
-                loadPackageParam.classLoader,
-                "onCreateView",
-                LayoutInflater.class, ViewGroup.class, android.os.Bundle.class,
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        View rootView = (View) param.getResult();
-                        Context context = rootView.getContext();
-
-                        RelativeLayout layout = new RelativeLayout(context);
-                        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
-                                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                        layout.setLayoutParams(layoutParams);
-
-
-                        Switch switchView = new Switch(context);
-                        switchView.setText("");
-
-                        RelativeLayout.LayoutParams switchParams = new RelativeLayout.LayoutParams(
-                                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                        switchParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE); // 中央に配置
-
-                        switchView.setChecked(false);
-                        switchView.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                            keepUnread = isChecked;
-                        });
-                        layout.addView(switchView, switchParams);
-                        if (rootView instanceof ViewGroup) {
-                            ViewGroup rootViewGroup = (ViewGroup) rootView;
-                            if (rootViewGroup.getChildCount() > 0 && rootViewGroup.getChildAt(0) instanceof ListView) {
-                                ListView listView = (ListView) rootViewGroup.getChildAt(0);
-                                listView.addFooterView(layout); // ListViewにフッターとして追加
-                            } else {
-                                rootViewGroup.addView(layout); // 他のビューに追加
-                            }
-                        }
-                    }
-
-                }
-        );
-
-
-        XposedHelpers.findAndHookMethod(
-                loadPackageParam.classLoader.loadClass(Constants.MARK_AS_READ_HOOK.className),
-                Constants.MARK_AS_READ_HOOK.methodName,
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) {
-
-                        if (keepUnread) {
-                            param.setResult(null);
+                    if (rootView instanceof ViewGroup) {
+                        ViewGroup rootViewGroup = (ViewGroup) rootView;
+                        if (rootViewGroup.getChildCount() > 0 && rootViewGroup.getChildAt(0) instanceof ListView) {
+                            ListView listView = (ListView) rootViewGroup.getChildAt(0);
+                            listView.addFooterView(layout);
+                        } else {
+                            rootViewGroup.addView(layout);
                         }
                     }
                 }
 
-        );
-    }
+                private void saveStateToFile(Context context, boolean state) {
+                    String filename = "keep_unread_state.txt";
+                    try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE)) {
+                        fos.write((state ? "1" : "0").getBytes());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                private boolean readStateFromFile(Context context) {
+                    String filename = "keep_unread_state.txt";
+                    try (FileInputStream fis = context.openFileInput(filename)) {
+                        int c;
+                        StringBuilder sb = new StringBuilder();
+                        while ((c = fis.read()) != -1) {
+                            sb.append((char) c);
+                        }
+                        return "1".equals(sb.toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+                }
+            }
+    );
+
+    XposedHelpers.findAndHookMethod(
+            loadPackageParam.classLoader.loadClass(Constants.MARK_AS_READ_HOOK.className),
+            Constants.MARK_AS_READ_HOOK.methodName,
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) {
+                    if (keepUnread) {
+                        param.setResult(null); 
+                    }
+                }
+            }
+    );
 }
