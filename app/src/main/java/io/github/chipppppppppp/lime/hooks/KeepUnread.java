d020fcbbcf07aaf144c75a238c21a7131c650297
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2024-11-03 21:48:06 +0900

    Update KeepUnread.java

--- app/src/main/java/io/github/chipppppppppp/lime/hooks/KeepUnread.java
+++ app/src/main/java/io/github/chipppppppppp/lime/hooks/KeepUnread.java
@@ -1,7 +1,7 @@
 package io.github.chipppppppppp.lime.hooks;
 
 import android.content.Context;
-
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.view.View;
@@ -11,10 +11,6 @@ import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
@@ -23,52 +19,78 @@ import io.github.chipppppppppp.lime.LimeOptions;
 import io.github.chipppppppppp.lime.R;
 
 public class KeepUnread implements IHook {
+    static boolean keepUnread = false;
 
     @Override
     public void hook(LimeOptions limeOptions, XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         if (limeOptions.removeKeepUnread.checked) return;
 
-        Class<?> hookTarget;
-        hookTarget = loadPackageParam.classLoader.loadClass("jp.naver.line.android.common.view.listview.PopupListView");
-        XposedBridge.hookAllConstructors(hookTarget, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        XposedBridge.hookAllConstructors(
+                loadPackageParam.classLoader.loadClass("jp.naver.line.android.common.view.listview.PopupListView"),
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        ViewGroup viewGroup = (ViewGroup) param.thisObject;
+                        Context context = viewGroup.getContext();
 
-                ViewGroup viewGroup = (ViewGroup) param.thisObject;
-                Context context = viewGroup.getContext();
-                Context moduleContext = context.getApplicationContext().createPackageContext(Constants.MODULE_NAME, Context.CONTEXT_IGNORE_SECURITY);
-                String textKeepUnread = moduleContext.getResources().getString(R.string.switch_keep_unread);
-                RelativeLayout layout = new RelativeLayout(context);
-                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
-                layout.setLayoutParams(layoutParams);
+                        SharedPreferences prefs = context.getSharedPreferences(Constants.MODULE_NAME + "-options", Context.MODE_PRIVATE);
 
-                Switch switchView = new Switch(context);
-                switchView.setText(textKeepUnread); 
-                RelativeLayout.LayoutParams switchParams = new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                switchParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                        Context moduleContext = context.getApplicationContext().createPackageContext(Constants.MODULE_NAME, Context.CONTEXT_IGNORE_SECURITY);
+                        String textKeepUnread = moduleContext.getResources().getString(R.string.switch_keep_unread);
 
-                switchView.setChecked(false);
-                switchView.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                });
+                        RelativeLayout container = new RelativeLayout(context);
+                        RelativeLayout.LayoutParams containerParams = new RelativeLayout.LayoutParams(
+                                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                        container.setLayoutParams(containerParams);
 
-                layout.addView(switchView, switchParams);
+                        GradientDrawable background = new GradientDrawable();
+                        background.setShape(GradientDrawable.RECTANGLE);
+                        background.setColor(Color.parseColor("#06C755"));
+                        background.setCornerRadii(new float[]{100, 100, 80, 30, 100, 100, 80, 30});
 
-                ((ListView) viewGroup.getChildAt(0)).addFooterView(layout);
-            }
-        });
-        
+                        container.setBackground(background);
+
+                        TextView label = new TextView(context);
+                        label.setText(textKeepUnread);
+                        label.setTextSize(18);
+                        label.setTextColor(Color.WHITE);
+                        label.setId(View.generateViewId());
+                        RelativeLayout.LayoutParams labelParams = new RelativeLayout.LayoutParams(
+                                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                        labelParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+                        labelParams.setMargins(40, 0, 0, 0);
+                        container.addView(label, labelParams);
+
+                        Switch switchView = new Switch(context);
+                        RelativeLayout.LayoutParams switchParams = new RelativeLayout.LayoutParams(
+                                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                        switchParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                        switchParams.setMargins(0, 0, 40, 0);
+                        keepUnread = prefs.getBoolean("keep_unread", false);
+                        switchView.setChecked(keepUnread);
+                        switchView.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                            keepUnread = isChecked;
+                            prefs.edit().putBoolean("keep_unread", isChecked).apply();
+                        });
+
+                        container.addView(switchView, switchParams);
+
+                        ((ListView) viewGroup.getChildAt(0)).addFooterView(container);
+                    }
+                }
+        );
+
         XposedHelpers.findAndHookMethod(
                 loadPackageParam.classLoader.loadClass(Constants.MARK_AS_READ_HOOK.className),
                 Constants.MARK_AS_READ_HOOK.methodName,
                 new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        if (keepUnread) {
                             param.setResult(null);
+                        }
                     }
                 }
         );
     }
-
 }
