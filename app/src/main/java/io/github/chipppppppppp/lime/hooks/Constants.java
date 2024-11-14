1ff18248d6c376375be550ea91e972a3e175445a
areteruhiro <narumihiroki1114@gmail.com>
2024-11-15 00:03:45 +0900

    開発者用のコードの追加

--- app/src/main/java/io/github/chipppppppppp/lime/hooks/Constants.java
+++ app/src/main/java/io/github/chipppppppppp/lime/hooks/Constants.java
@@ -19,7 +19,9 @@ public class Constants {
     static final HookTarget MUTE_MESSAGE_HOOK = new HookTarget("u71.b", "H");
     static final HookTarget MARK_AS_READ_HOOK = new HookTarget("JL.e$d", "run");
 
+    //以下のクラス名はtestメゾットで出力させることが出来ます、(14.17.0になっています)
     static final HookTarget NOTIFICATION_READ_HOOK = new HookTarget("H91.c", "invokeSuspend");
+
     static final HookTarget REQUEST_HOOK = new HookTarget("org.apache.thrift.k", "b");
     static final HookTarget RESPONSE_HOOK = new HookTarget("org.apache.thrift.k", "a");
 }
