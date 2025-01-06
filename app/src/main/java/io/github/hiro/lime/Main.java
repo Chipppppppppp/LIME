a7a20186146493179e5fe7888965f607437c1490
areteruhiro <narumihiroki1114@gmail.com>
2025-01-06 21:51:38 +0900

    既読機能についての修正(確認してから作成する)

--- app/src/main/java/io/github/hiro/lime/Main.java
+++ app/src/main/java/io/github/hiro/lime/Main.java
@@ -37,7 +37,7 @@ import io.github.hiro.lime.hooks.RemoveIcons;
 import io.github.hiro.lime.hooks.RemoveNotification;
 import io.github.hiro.lime.hooks.RemoveReplyMute;
 import io.github.hiro.lime.hooks.RemoveVoiceRecord;
-import io.github.hiro.lime.hooks.Resend;
+
 import io.github.hiro.lime.hooks.Ringtone;
 import io.github.hiro.lime.hooks.SendMuteMessage;
 import io.github.hiro.lime.hooks.SpoofAndroidId;
@@ -46,7 +46,7 @@ import io.github.hiro.lime.hooks.UnsentRec;
 import io.github.hiro.lime.hooks.Archived;
 import io.github.hiro.lime.hooks.ReadChecker;
 import io.github.hiro.lime.hooks.DarkColor;
-import io.github.hiro.lime.hooks.test;
+
 
 public class Main implements IXposedHookLoadPackage, IXposedHookInitPackageResources, IXposedHookZygoteInit {
     public static String modulePath;
@@ -88,7 +88,7 @@ public class Main implements IXposedHookLoadPackage, IXposedHookInitPackageResou
             new Disabled_Group_notification(),
             new PhotoAddNotification(),
             new RemoveVoiceRecord(),
-            new Resend()
+
     };
 
     public void handleLoadPackage(@NonNull XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
