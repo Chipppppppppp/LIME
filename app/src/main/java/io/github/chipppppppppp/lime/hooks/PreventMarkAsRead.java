2b765a254eb14ead85d5d2956bfe8b76b52cdf06
Chippppp <78024852+Chipppppppppp@users.noreply.github.com>
2025-01-20 00:40:25 +0900

    Prevent crash of PreventMarkAsRead

--- app/src/main/java/io/github/chipppppppppp/lime/hooks/PreventMarkAsRead.java
+++ app/src/main/java/io/github/chipppppppppp/lime/hooks/PreventMarkAsRead.java
@@ -17,7 +17,7 @@ public class PreventMarkAsRead implements IHook {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                         if (param.args[0].toString().equals("sendChatChecked")) {
-                            param.setResult(null);
+                            param.args[0] = param.args[0].getClass().getMethod("valueOf", String.class).invoke(null, "DUMMY");
                         }
                     }
                 }
