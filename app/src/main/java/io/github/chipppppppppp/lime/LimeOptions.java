0ebc8000185e686a827ab322a9c1fc5adcca0f14
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-01-24 12:41:25 +0900

    LimeOptions.java の更新

--- app/src/main/java/io/github/chipppppppppp/lime/LimeOptions.java
+++ app/src/main/java/io/github/chipppppppppp/lime/LimeOptions.java
@@ -13,19 +13,19 @@ public class LimeOptions {
         }
     }
 
-    public Option removeVoom = new Option("remove_voom", R.string.switch_remove_voom, true);
-    public Option removeWallet = new Option("remove_wallet", R.string.switch_remove_wallet, true);
-    public Option removeNewsOrCall = new Option("remove_news_or_call", R.string.switch_remove_news_or_call, true);
-    public Option distributeEvenly = new Option("distribute_evenly", R.string.switch_distribute_evenly, true);
-    public Option extendClickableArea = new Option("extend_clickable_area", R.string.switch_extend_clickable_area, true);
-    public Option removeIconLabels = new Option("remove_icon_labels", R.string.switch_remove_icon_labels, true);
-    public Option removeAds = new Option("remove_ads", R.string.switch_remove_ads, true);
-    public Option removeRecommendation = new Option("remove_recommendation", R.string.switch_remove_recommendation, true);
-    public Option removePremiumRecommendation = new Option("remove_premium_recommendation", R.string.switch_remove_premium_recommendation, true);
+    public Option removeVoom = new Option("remove_voom", R.string.switch_remove_voom, false);
+    public Option removeWallet = new Option("remove_wallet", R.string.switch_remove_wallet, false);
+    public Option removeNewsOrCall = new Option("remove_news_or_call", R.string.switch_remove_news_or_call, false);
+    public Option distributeEvenly = new Option("distribute_evenly", R.string.switch_distribute_evenly, false);
+    public Option extendClickableArea = new Option("extend_clickable_area", R.string.switch_extend_clickable_area, false);
+    public Option removeIconLabels = new Option("remove_icon_labels", R.string.switch_remove_icon_labels, false);
+    public Option removeAds = new Option("remove_ads", R.string.switch_remove_ads, false);
+    public Option removeRecommendation = new Option("remove_recommendation", R.string.switch_remove_recommendation, false);
+    public Option removePremiumRecommendation = new Option("remove_premium_recommendation", R.string.switch_remove_premium_recommendation, false);
     public Option removeServiceLabels = new Option("remove_service_labels", R.string.switch_remove_service_labels, false);
     public Option removeAllServices = new Option("remove_services", R.string.switch_remove_service, false);
     public Option removeReplyMute = new Option("remove_reply_mute", R.string.switch_remove_reply_mute, true);
-    public Option redirectWebView = new Option("redirect_webview", R.string.switch_redirect_webview, true);
+    public Option redirectWebView = new Option("redirect_webview", R.string.switch_redirect_webview, false);
     public Option openInBrowser = new Option("open_in_browser", R.string.switch_open_in_browser, false);
     public Option preventMarkAsRead = new Option("prevent_mark_as_read", R.string.switch_prevent_mark_as_read, false);
     public Option preventUnsendMessage = new Option("prevent_unsend_message", R.string.switch_prevent_unsend_message, false);
